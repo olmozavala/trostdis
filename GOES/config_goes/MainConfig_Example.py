c1fd5b696ff72ce88afc69f7eff49855d957a7d1
jorgeev <42640851+jorgeev@users.noreply.github.com>
2020-04-07 15:47:01 -0500

    Updated paths

--- GOES/config_goes/MainConfig_Example.py
+++ GOES/config_goes/MainConfig_Example.py
@@ -2,9 +2,9 @@ from config_goes.params import GOES
 
 def get_config():
     cur_config = {
-        GOES.band: '6',
+        GOES.band: '4',
         GOES.input_folder: '../test_data/goes',
-        GOES.output_folder: 'output_goes'
+        GOES.output_folder: '../test_data/goes/output'
     }
     return cur_config
 
