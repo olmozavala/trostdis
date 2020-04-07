cf79a9e80c871733282faeae239cb2a881156a27
jorgeev <42640851+jorgeev@users.noreply.github.com>
2020-04-07 13:24:04 -0500

    Updated Example to use CSV

--- GOES/config_goes/ClassConfig_Example.py
+++ GOES/config_goes/ClassConfig_Example.py
@@ -4,6 +4,6 @@ def get_config():
     cur_config = {
         CLASS.user0: 'rk.ecmwf',
         CLASS.pass0: 'BI755N0txW',
-        CLASS.dates: '../test_data/goes/Ernesto.npz'
+        CLASS.dates: '../test_data/goes/Hurdat_date.csv' # CSV in format '%Y-%m-%d %H:%M:%S'
     }
     return cur_config
